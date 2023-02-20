0c8e75069c624a9da865195a1c262a78395f8aef
Remy Blank <remy@c-space.org>
2023-02-20 15:03:50 +0100

    Add helpers for object-oriented programming.

---
+++ lib/class.lua
@@ -0,0 +1,32 @@
+-- This module provides helpers for object-oriented programming.
+
+-- The __call metamethod of all classes.
+local function class_call(cls, ...)
+    local obj = setmetatable({}, cls)
+    local init = cls.__init
+    if init then
+        init(obj, ...)
+    end
+    return obj
+end
+
+-- Define a class, optionally inheriting from the given base.
+function class(base)
+    local cls = {__base = base}
+    cls.__index = cls
+    return setmetatable(cls, {__index = base, __call = class_call})
+end
+
+-- Return true iff "obj" is an instance of "cls".
+function isinstance(obj, cls)
+    return issubclass(getmetatable(obj), cls)
+end
+
+-- Return true iff "cls" is a subclass of "base".
+function issubclass(cls, base)
+    while cls do
+        if cls == base then return true end
+        cls = cls.__base
+    end
+    return false
+end
