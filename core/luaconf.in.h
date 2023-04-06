504befb0884feb8a532a48e3b458718b0361fa2b
Remy Blank <remy@c-space.org>
2023-04-06 21:09:46 +0200

    Fix an issue with formatting floats with %q.

--- core/luaconf.in.h
+++ core/luaconf.in.h
@@ -13,3 +13,10 @@
 
 #undef LUAI_MAXSTACK
 #define LUAI_MAXSTACK       1000
+
+// Lua uses the C99 %a printf specifier in lua_number2strx. The "pico" printf
+// implementation (see pico_set_printf_implementation) doesn't support %a. The
+// "compiler" implementation uses newlib, which does support %a but only if it
+// is compiled with --enable-newlib-io-c99-formats. Lua provides a fallback if
+// lua_number2strx is undefined.
+#undef lua_number2strx
