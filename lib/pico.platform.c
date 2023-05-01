00ba2299ca040221ef61a4db16019c882de002ee
Remy Blank <remy@c-space.org>
2023-05-01 19:37:29 +0200

    Export more pico_platform symbols.

--- lib/pico.platform.c
+++ lib/pico.platform.c
@@ -17,6 +17,26 @@ static mlua_reg const module_regs[] = {
     X(busy_wait_at_least_cycles),
     X(get_core_num),
 #undef X
+#define X(n) {.name=#n, .push=mlua_reg_push_integer, .integer=n}
+    X(NUM_CORES),
+    X(NUM_DMA_CHANNELS),
+    X(NUM_DMA_TIMERS),
+    X(NUM_IRQS),
+    X(NUM_USER_IRQS),
+    X(NUM_PIOS),
+    X(NUM_PIO_STATE_MACHINES),
+    X(NUM_PWM_SLICES),
+    X(NUM_SPIN_LOCKS),
+    X(NUM_UARTS),
+    X(NUM_I2CS),
+    X(NUM_SPIS),
+    X(NUM_TIMERS),
+    X(NUM_ADC_CHANNELS),
+    X(NUM_BANK0_GPIOS),
+    X(NUM_QSPI_GPIOS),
+    X(PIO_INSTRUCTION_COUNT),
+    X(FIRST_USER_IRQ),
+#undef X
 #define X(n) MLUA_REG(integer, n, PICO_ ## n)
 #ifdef PICO_RP2040
     X(RP2040),
