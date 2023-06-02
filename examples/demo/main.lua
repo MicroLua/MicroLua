fc5715945556af8264011b10bb59d5167bfc634c
Remy Blank <remy@c-space.org>
2023-06-02 11:50:49 +0200

    Reorganize demo.

--- examples/demo/main.lua
+++ examples/demo/main.lua
@@ -17,33 +17,7 @@ local time = require 'pico.time'
 local unique_id = require 'pico.unique_id'
 local string = require 'string'
 
-local irq_num = irq.user_irq_claim_unused()
-
-local function task(id, start)
-    thread.yield(start)
-    for i = 0, 2 do
-        eio.printf("Task: %s (%s), i: %s, t: %s\n", id, thread.running(), i,
-                   thread.now())
-        -- debug.debug()
-        irq.set_pending(irq_num)
-        thread.yield(start + (i + 1) * 1000000)
-    end
-    eio.printf("Task: %s, done\n", id)
-end
-
-function main()
-    -- Switch the system clock.
-    stdlib.set_sys_clock_khz(200000, true)
-    -- stdlib.set_sys_clock_48mhz()
-    stdlib.setup_default_uart()
-
-    -- Wait for the system timer to start (it takes a while).
-    timer.busy_wait_us(1)
-    local start = time.get_absolute_time()
-    eio.printf(string.rep("=", 79) .. "\n")
-
-    -- Print system information.
-    eio.printf("Start time: %s\n", start)
+local function demo_sysinfo()
     eio.printf("Chip version: %d\n", platform.rp2040_chip_version())
     eio.printf("ROM version: %d\n", platform.rp2040_rom_version())
     local id = unique_id.get_unique_board_id()
@@ -52,8 +26,9 @@ function main()
         unique_id.get_unique_board_id_string())
     eio.printf("Core: %d\n", platform.get_core_num())
     eio.printf("SDK: %s\n", pico.SDK_VERSION_STRING)
+end
 
-    -- Measure clocks.
+local function demo_clocks()
     eio.printf("pll_sys : %6d kHz\n", clocks.frequency_count_khz(
         clocks.FC0_SRC_VALUE_PLL_SYS_CLKSRC_PRIMARY))
     eio.printf("pll_usb : %6d kHz\n", clocks.frequency_count_khz(
@@ -70,12 +45,18 @@ function main()
         clocks.FC0_SRC_VALUE_CLK_ADC))
     eio.printf("clk_rtc : %6d kHz\n", clocks.frequency_count_khz(
         clocks.FC0_SRC_VALUE_CLK_RTC))
+end
 
-    -- Launch core 1.
-    -- multicore.launch_core1('main1')
-    local core1_running = true
+local function demo_core1()
+    multicore.launch_core1('main1')
+    thread.start(function()
+        thread.yield(thread.now() + 5000000)
+        eio.printf("# Resetting core 1\n")
+        multicore.reset_core1()
+    end)
+end
 
-    -- Exercise timers.
+local function demo_hardware_timer()
     local next_time = time.get_absolute_time() + 1500000
     next_time = next_time - (next_time % 1000000)
     timer.set_callback(0, function(alarm)
@@ -83,27 +64,45 @@ function main()
         next_time = next_time + 1000000
         timer.set_target(0, next_time)
         eio.printf("# Alarm at %s, next at %s\n", now, next_time)
-        if core1_running and now - start > 5000000 then
-            eio.printf("# Resetting core 1\n")
-            multicore.reset_core1()
-            core1_running = false
+    end)
+    timer.set_target(0, next_time)
+end
+
+local function demo_user_irq()
+    local irq_num = irq.user_irq_claim_unused()
+    irq.set_handler(irq_num, function() eio.printf("# User IRQ\n") end)
+    irq.clear(irq_num)
+    irq.set_enabled(irq_num, true)
+    thread.start(function()
+        thread.yield(thread.now() + 500000)
+        for i = 0, 2 do
+            irq.set_pending(irq_num)
+            thread.yield(thread.now() + 500000)
         end
     end)
-    -- timer.set_target(0, next_time)
+end
 
-    -- Set a user IRQ handler.
-    irq.set_handler(irq_num, function() eio.printf("# IRQ!\n") end)
-    irq.clear(irq_num)
-    -- irq.set_enabled(irq_num, true)
 
-    -- Start a few threads.
+local function task(id, start)
+    thread.yield(start)
+    for i = 0, 2 do
+        eio.printf("Task: %s (%s), i: %s, t: %s\n", id, thread.running(), i,
+                   thread.now())
+        -- debug.debug()
+        thread.yield(start + (i + 1) * 1000000)
+    end
+    eio.printf("Task: %s, done\n", id)
+end
+
+local function demo_threads()
     local base = thread.now() + 1500000
     base = base - (base % 1000000)
     for i = 0, 3 do
-        -- thread.start(function() task(i, base + i * 250000) end)
+        thread.start(function() task(i, base + i * 250000) end)
     end
+end
 
-    -- Exercise GPIOs.
+local function demo_gpio()
     local led = 21
     gpio.init(led)
     gpio.set_dir(led, gpio.OUT)
@@ -116,8 +115,9 @@ function main()
     gpio.set_irq_enabled(button, gpio.IRQ_EDGE_FALL | gpio.IRQ_EDGE_RISE, true)
     -- gpio.set_irq_enabled(button, gpio.IRQ_LEVEL_HIGH, true)
     irq.set_enabled(irq.IO_IRQ_BANK0, true)
+end
 
-    -- Exercise IRQ-driven stdin reception.
+local function demo_stdin()
     local line = ''
     stdio.set_chars_available_callback(function()
         line = line .. stdin:read(32)  -- UART Rx FIFO is 32 bytes
@@ -128,4 +128,26 @@ function main()
             line = line:sub(i + 1)
         end
     end)
+end
+
+function main()
+    -- Set the system clock.
+    stdlib.set_sys_clock_khz(200000, true)
+    -- stdlib.set_sys_clock_48mhz()
+    stdlib.setup_default_uart()
+
+    -- Wait for the system timer to start (it takes a while).
+    timer.busy_wait_us(1)
+    local start = time.get_absolute_time()
+    eio.printf(string.rep("=", 79) .. "\n")
+    eio.printf("Start time: %s\n", start)
+
+    demo_sysinfo()
+    demo_clocks()
+    -- demo_core1()
+    -- demo_hardware_timer()
+    -- demo_user_irq()
+    -- demo_threads()
+    demo_gpio()
+    demo_stdin()
 end
