241262af85d69036a35a9a603e18334b62bc838f
Remy Blank <remy@c-space.org>
2023-06-23 15:07:58 +0200

    Add tests for deferreds.

--- lib/mlua.util.test.lua
+++ lib/mlua.util.test.lua
@@ -1,4 +1,21 @@
 _ENV = require 'mlua.module'(...)
 
-function test_something(t)
+function test_Function_close(t)
+    local called = false
+    do
+        local f<close> = function() called = true end
+    end
+    t:expect(called, "To-be-closed function wasn't called")
+
+    local called = false
+    pcall(function()
+        local want = "Some error"
+        local f<close> = function(err)
+            called = true
+            t:expect(err == want,
+                     "Unexpected error: got %q, want %q", err, want)
+        end
+        error(want, 0)
+    end)
+    t:expect(called, "To-be-closed function wasn't called on error")
 end
