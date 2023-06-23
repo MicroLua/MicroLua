6ac558f1a2d951e46615f67425a90190c8a66530
Remy Blank <remy@c-space.org>
2023-06-24 00:08:14 +0200

    Add tests for hardware.clocks.

--- lib/hardware.clocks.test.lua
+++ lib/hardware.clocks.test.lua
@@ -1,4 +1,25 @@
 _ENV = require 'mlua.module'(...)
 
-function test_something(t)
+local clocks = require 'hardware.clocks'
+local table = require 'table'
+
+function test_frequency(t)
+    for _, test in ipairs{
+        {'FC0_SRC_VALUE_PLL_SYS_CLKSRC_PRIMARY', 'clk_sys', 0.0001},
+        {'FC0_SRC_VALUE_PLL_USB_CLKSRC_PRIMARY', 'clk_usb', 0.0001},
+        {'FC0_SRC_VALUE_ROSC_CLKSRC', 6500.0, 0.85},
+        {'FC0_SRC_VALUE_CLK_SYS', 'clk_sys', 0.0001},
+        {'FC0_SRC_VALUE_CLK_PERI', 'clk_peri', 0.0001},
+        {'FC0_SRC_VALUE_CLK_USB', 'clk_usb', 0.0001},
+        {'FC0_SRC_VALUE_CLK_ADC', 'clk_adc', 0.0001},
+        {'FC0_SRC_VALUE_CLK_RTC', 'clk_rtc', 0.03},
+    } do
+        local clk, want, tol = table.unpack(test)
+        if type(want) == 'string' then
+            want = clocks.get_hz(clocks[want]) / 1000
+        end
+        local got = clocks.frequency_count_khz(clocks[clk])
+        t:expect((1.0 - tol) * want <= got and got <= (1.0 + tol) * want,
+                 "freq(%s) = %s, want %.0f", clk, got, want)
+    end
 end
