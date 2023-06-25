36210e2e4da8ab0df1acf68bf050c35bfeb5ae03
Remy Blank <remy@c-space.org>
2023-06-25 20:50:58 +0200

    Add tests for hardware.irq.

--- lib/hardware.irq.test.lua
+++ lib/hardware.irq.test.lua
@@ -1,4 +1,44 @@
 _ENV = mlua.Module(...)
 
-function test_something(t)
+local irq = require 'hardware.irq'
+local thread = require 'mlua.thread'
+local util = require 'mlua.util'
+local string = require 'string'
+
+function test_user_irqs(t)
+    local rounds = 10
+
+    -- Claim user IRQs.
+    local irqs, nums = {}, ''
+    for i = 1, irq.NUM_USER_IRQS do
+        local num = irq.user_irq_claim_unused()
+        irq.clear(num)
+        irq.enable_user_irq(num)
+        t:cleanup(function()
+            irq.enable_user_irq(num, false)
+            irq.user_irq_unclaim(num)
+        end)
+        irqs = util.append(irqs, num)
+        nums = nums .. ('%s '):format(num)
+    end
+
+    -- Set up IRQ handlers.
+    local log
+    local handlers<close> = thread.Group()
+    for _, num in ipairs(irqs) do
+        handlers:start(function()
+            for i = 1, rounds do
+                irq.wait_user_irq(num)
+                log = log .. ('%s '):format(num)
+            end
+        end)
+    end
+
+    -- Trigger IRQs.
+    for i = 1, rounds do
+        log = ''
+        for _, num in ipairs(irqs) do irq.set_pending(num) end
+        thread.yield()
+        t:expect(log == nums, "Unexpected sequence on round %s: %s", i, log)
+    end
 end
