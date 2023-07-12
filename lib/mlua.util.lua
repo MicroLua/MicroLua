a224c96d445bf6338ade4dba93df8f9a861c02b1
Remy Blank <remy@c-space.org>
2023-07-12 14:48:46 +0200

    Add tests for mlua.util.

--- lib/mlua.util.lua
+++ lib/mlua.util.lua
@@ -2,15 +2,33 @@
 
 _ENV = mlua.Module(...)
 
+local string = require 'string'
 local table = require 'table'
-
--- TODO: Refactor the functionality below as iterators
 
 -- Return all arguments.
 function ident(...) return ... end
 
 -- Return true iff a == b.
 function eq(a, b) return a == b end
+
+-- Return a string representation of the given value.
+function str(v)
+    local typ = type(v)
+    if typ == 'string' then return ('%q'):format(v)
+    elseif typ ~= 'table' then return tostring(v) end
+    local parts = {}
+    for k, v in pairs(v) do
+        append(parts, ('[%s] = %s'):format(str(k), str(v)))
+    end
+    table.sort(parts)
+    return '{' .. table.concat(parts, ', ') .. '}'
+end
+
+-- Return an object whose __tostring metamethod calls a function (str by
+-- default) on its argument.
+function lstr(v, fn)
+    return setmetatable({}, {__tostring = function() return (fn or str)(v) end})
+end
 
 -- Return the length of a list.
 function len(list) return not list and 0 or list[0] or #list end
@@ -26,25 +44,27 @@ end
 
 -- Return the keys of the given table, optionally filtered.
 function keys(tab, filter)
-    local res, i = {}, 1
+    local res, len = {}, 0
     for key, value in pairs(tab) do
         if not filter or filter(key, value) then
-            res[i] = key
-            i = i + 1
+            len = len + 1
+            res[len] = key
         end
     end
+    res[0] = len
     return res
 end
 
 -- Return the values of the given table, optionally filtered.
 function values(tab, filter)
-    local res, i = {}, 1
+    local res, len = {}, 0
     for key, value in pairs(tab) do
         if not filter or filter(key, value) then
-            res[i] = value
-            i = i + 1
+            len = len + 1
+            res[len] = value
         end
     end
+    res[0] = len
     return res
 end
 
@@ -52,6 +72,17 @@ end
 function sort(list, comp)
     table.sort(list, comp)
     return list
+end
+
+-- Return true iff the key / value pairs of the given tables compare equal.
+function table_eq(a, b)
+    for k, v in pairs(a) do
+        if b[k] ~= v then return false end
+    end
+    for k in pairs(b) do
+        if a[k] == nil then return false end
+    end
+    return true
 end
 
 -- Return a table comparator comparing table pairs by the elements at the given
--- lib/mlua.util.test.lua
+++ lib/mlua.util.test.lua
@@ -1,4 +1,132 @@
 _ENV = mlua.Module(...)
 
-function test_something(t)
+local util = require 'mlua.util'
+local table = require 'table'
+
+function test_str(t)
+    for _, test in ipairs{
+        {nil, 'nil'},
+        {123, '123'},
+        {4.5, '4.500000'},
+        {'abc', '"abc"'},
+        {true, 'true'},
+        {{}, '{}'},
+        {{1, 2}, '{[1] = 1, [2] = 2}'},
+        {{a = 1, b = 2}, '{["a"] = 1, ["b"] = 2}'},
+        {{a = {1, 2}, b = {c = 3, d = 4}},
+         '{["a"] = {[1] = 1, [2] = 2}, ["b"] = {["c"] = 3, ["d"] = 4}}'},
+    } do
+        local v, want = table.unpack(test)
+        local got = util.str(v)
+        t:expect(got == want, "str(%s) = %s, want %s", v, got, want)
+        local got = tostring(util.lstr(v))
+        t:expect(got == want, "lstr(%s) = %s, want %s", v, got, want)
+    end
+end
+
+function test_len(t)
+    for _, test in ipairs{
+        {nil, 0},
+        {{}, 0},
+        {{1, 2, 3}, 3},
+        {{[0] = 7}, 7},
+    } do
+        local list, want = table.unpack(test)
+        local got = util.len(list)
+        t:expect(got == want,
+                 "len(%s) = %s, want %s", util.lstr(list), got, want)
+    end
+end
+
+function test_append(t)
+    for _, test in ipairs{
+        {nil, 1, {[0] = 1, 1}},
+        {{}, 1, {[0] = 1, 1}},
+        {{1, 2}, 3, {[0] = 3, 1, 2, 3}},
+        {{[0] = 2, 1, 2}, 3, {[0] = 3, 1, 2, 3}},
+    } do
+        local list, el, want = table.unpack(test)
+        local got = util.append(list, el)
+        t:expect(util.table_eq(got, want),
+                 "append(%s) = %s, want %s", util.lstr(list), util.lstr(got),
+                 util.lstr(want))
+    end
+end
+
+function test_keys(t)
+    for _, test in ipairs{
+        {{}, nil, {[0] = 0}},
+        {{a = 1, b = 2, c = 3}, nil, {[0] = 3, 'a', 'b', 'c'}},
+        {{a = 4, b = 5, c = 6}, function(k, v) return k ~= 'b' and v ~= 4 end,
+         {[0] = 1, 'c'}},
+    } do
+        local tab, filter, want = table.unpack(test)
+        local got = util.keys(tab, filter)
+        table.sort(got)
+        t:expect(util.table_eq(got, want),
+                 "keys(%s) = %s, want %s", util.lstr(tab), util.lstr(got),
+                 util.lstr(want))
+    end
+end
+
+function test_values(t)
+    for _, test in ipairs{
+        {{}, nil, {[0] = 0}},
+        {{a = 1, b = 2, c = 3}, nil, {[0] = 3, 1, 2, 3}},
+        {{a = 4, b = 5, c = 6}, function(k, v) return k ~= 'b' and v ~= 4 end,
+         {[0] = 1, 6}},
+    } do
+        local tab, filter, want = table.unpack(test)
+        local got = util.values(tab, filter)
+        table.sort(got)
+        t:expect(util.table_eq(got, want),
+                 "values(%s) = %s, want %s", util.lstr(tab), util.lstr(got),
+                 util.lstr(want))
+    end
+end
+
+function test_sort(t)
+    for _, test in ipairs{
+        {{}, nil, {}},
+        {{1, 4, 2, 8, 5}, nil, {1, 2, 4, 5, 8}},
+        {{[0] = 5, 1, 4, 2, 8, 5}, nil, {[0] = 5, 1, 2, 4, 5, 8}},
+        {{1, 4, 2, 8, 5}, function(a, b) return a > b end, {8, 5, 4, 2, 1}},
+    } do
+        local list, comp, want = table.unpack(test)
+        local got = util.sort(list, comp)
+        t:expect(util.table_eq(got, want),
+                 "sort(%s) = %s, want %s", util.lstr(list), util.lstr(got),
+                 util.lstr(want))
+    end
+end
+
+function test_table_eq(t)
+    for _, test in ipairs{
+        {{}, {}, true},
+        {{1, 2}, {}, false},
+        {{}, {1, 2}, false},
+        {{1, 2}, {1, 3}, false},
+        {{1, 2}, {1, 2}, true},
+        {{a = 1, b = 2}, {a = 1, b = 2}, true},
+        {{a = 1, b = 2}, {a = 1, c = 2}, false},
+        {{a = 1, b = 2}, {a = 1, b = 3}, false},
+    } do
+        local a, b, want = table.unpack(test)
+        local got = util.table_eq(a, b)
+        t:expect(got == want, "table_eq(%s, %s) = %s, want %s", util.lstr(a),
+                 util.lstr(b), got, want)
+    end
+end
+
+function test_table_comp(t)
+    local a, b = {10, 20, 30}, {30, 20, 10}
+    for _, test in ipairs{
+        {{1}, true}, {{2}, false}, {{3}, false},
+        {{1, 2, 3}, true}, {{2, 2, 2}, false}, {{3, 2, 1}, false},
+    } do
+        local keys, want = table.unpack(test)
+        local got = util.table_comp(keys)(a, b)
+        t:expect(got == want,
+                 "table_comp(%s) = %s, want %s", util.lstr(keys), got, want)
+    end
 end
