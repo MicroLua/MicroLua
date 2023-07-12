57819a1869ee24826acee9a71daa0818a02950d2
Remy Blank <remy@c-space.org>
2023-07-12 10:17:18 +0200

    Add tests for mlua.oo.

--- lib/mlua.oo.lua
+++ lib/mlua.oo.lua
@@ -1,26 +1,41 @@
 -- A simple object model for object-oriented programming.
+--
+-- Classes are created with class, providing a name and optionally a base class.
+-- Instances are created by calling the class, optionally with arguments. The
+-- later are passed to the instance initializer __init, if it is defined.
+--
+-- Metamethods can be defined on classes, and have the expected effect. They are
+-- copied from the base class to the subclass at class creation time. This is
+-- necessary because Lua gets metamethods using a raw access.
 
 _ENV = mlua.Module(...)
 
--- The __call metamethod of all classes.
-local function class_call(cls, ...)
+-- The class of all classes.
+local Metaclass = {}
+
+function Metaclass.__tostring(cls) return 'class ' .. cls.__name end
+
+function Metaclass.__call(cls, ...)
     local obj = setmetatable({}, cls)
     local init = cls.__init
     if init then init(obj, ...) end
     return obj
 end
 
--- Define a class, optionally inheriting from the given base.
+-- Define a class, optionally inheriting from the given base class.
 function class(name, base)
-    local cls = name
-    if type(name) == 'string' then cls = {__name = name, __base = base} end
-    cls.__index = cls
-    return setmetatable(cls, {__index = cls.__base, __call = class_call})
-end
-
--- Return true iff "obj" is an instance of "cls".
-function isinstance(obj, cls)
-    return issubclass(getmetatable(obj), cls)
+    local cls = {}
+    if base then
+        for k, v in pairs(base) do
+            if k:sub(1, 2) == '__' then cls[k] = v end
+        end
+    end
+    cls.__name, cls.__base, cls.__index = name, base, cls
+    return setmetatable(cls, not base and Metaclass or {
+        __index = cls.__base,
+        __tostring = Metaclass.__tostring,
+        __call = Metaclass.__call,
+    })
 end
 
 -- Return true iff "cls" is a subclass of "base".
@@ -31,3 +46,6 @@ function issubclass(cls, base)
     end
     return false
 end
+
+-- Return true iff "obj" is an instance of "cls".
+function isinstance(obj, cls) return issubclass(getmetatable(obj), cls) end
--- lib/mlua.oo.test.lua
+++ lib/mlua.oo.test.lua
@@ -1,4 +1,81 @@
 _ENV = mlua.Module(...)
 
-function test_something(t)
+local oo = require 'mlua.oo'
+local string = require 'string'
+
+local A = oo.class('A')
+A.cls_a = 1
+
+function A:__init(a) self.a = a end
+function A:method() return self.a end
+function A:__add(other) return A(self.a + other.a) end
+function A:__len() return self.a end
+
+local B = oo.class('B', A)
+B.cls_b = 2
+
+function B:__init(a, b)
+    A.__init(self, a)
+    self.b = b
+end
+
+function test_class_tostring(t)
+    local got = tostring(A)
+    t:expect(got == 'class A', "tostring(A) = %q", got)
+    local got = tostring(B)
+    t:expect(got == 'class B', "tostring(B) = %q", got)
+end
+
+function test_instance_tostring(t)
+    local a, b = A(10), B(20, 30)
+    local got = tostring(a)
+    t:expect(got:find('^A: [0-9A-F]+$'), "tostring(a) = %q", got)
+    local got = tostring(b)
+    t:expect(got:find('^B: [0-9A-F]+$'), "tostring(b) = %q", got)
+end
+
+function test_class_attributes(t)
+    t:expect(A.__name == 'A', "A.__name = %s, want A", A.__name)
+    t:expect(B.__name == 'B', "B.__name = %s, want B", B.__name)
+    t:expect(A.__base == nil, "A.__base = %s, want nil", A.__base)
+    t:expect(B.__base == A, "B.__base = %s, want %s", B.__base, A)
+end
+
+function test_instance_attributes(t)
+    local a, b = A(10), B(20, 30)
+    t:expect(a.cls_a == 1, "a.cls_a = %s, want 1", a.cls_a)
+    t:expect(a.a == 10, "a.a = %s, want 10", a.a)
+    t:expect(b.cls_a == 1, "b.cls_a = %s, want 1", b.cls_a)
+    t:expect(b.cls_b == 2, "b.cls_b = %s, want 2", b.cls_b)
+    t:expect(b.a == 20, "b.a = %s, want 20", b.a)
+    t:expect(b.b == 30, "b.b = %s, want 30", b.b)
+end
+
+function test_methods(t)
+    local got = A(12):method()
+    t:expect(got == 12, "got %s, want 12", got)
+    local got = B(34, 56):method()
+    t:expect(got == 34, "got %s, want 34", got)
+end
+
+function test_metamethods(t)
+    local got = A(1) + A(2) + A(3)
+    t:expect(got.a == 6, "got.a = %s, want 6", got.a)
+    local got = #B(4, 5)
+    t:expect(got == 4, "got %s, want 4", got)
+end
+
+function test_issubclass(t)
+    t:expect(oo.issubclass(A, A), "issubclass(A, A) = false")
+    t:expect(oo.issubclass(B, B), "issubclass(B, B) = false")
+    t:expect(not oo.issubclass(A, B), "issubclass(A, B) = true")
+    t:expect(oo.issubclass(B, A), "issubclass(B, A) = false")
+end
+
+function test_isinstance(t)
+    local a, b = A(10), B(20, 30)
+    t:expect(oo.isinstance(a, A), "isinstance(a, A) = false")
+    t:expect(oo.isinstance(b, B), "isinstance(b, B) = false")
+    t:expect(not oo.isinstance(a, B), "isinstance(a, B) = true")
+    t:expect(oo.isinstance(b, A), "isinstance(b, A) = false")
 end
