5d33a89125fcfd7091d2ee66072b1fa9560e03df
Remy Blank <remy@c-space.org>
2023-07-29 01:58:18 +0200

    Implement proper shutdown of the interpreter in core 1.
     - For shutdown to work, core 1 must start a thread that waits for the
       shutdown event, then shuts down the scheduler.
    
     - reset_core1() waits for the interpreter in core 1 to have terminated
       before resetting the core.

--- lib/pico.multicore.c
+++ lib/pico.multicore.c
@@ -1,57 +1,222 @@
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "hardware/sync.h"
 #include "pico/multicore.h"
 #include "pico/platform.h"
+#include "pico/time.h"
 
 #include "lua.h"
 #include "lauxlib.h"
+#include "mlua/event.h"
+#include "mlua/int64.h"
 #include "mlua/main.h"
 #include "mlua/util.h"
 
-// TODO: Provide a way to shut down the Lua interpreter running in core1 and
-//       correctly release all resources.
+typedef struct CoreState {
+    lua_State* ls;
+    MLuaEvent shutdown_event;
+    MLuaEvent stopped_event;
+    bool shutdown;
+} CoreState;
 
-static lua_State* interpreters[NUM_CORES - 1];
+static CoreState core_state[NUM_CORES - 1];
 
-static void launch_core1(void) {
-    lua_State* ls = interpreters[get_core_num() - 1];
+static void launch_core(void) {
+    CoreState* st = &core_state[get_core_num() - 1];
+    lua_State* ls = st->ls;
     mlua_run_main(ls);
-    uint32_t save = spin_lock_blocking(mlua_lock);
-    interpreters[get_core_num() - 1] = NULL;
-    spin_unlock(mlua_lock, save);
+    mlua_event_unclaim(ls, &st->shutdown_event);
     lua_close(ls);
+    uint32_t save = mlua_event_lock();
+    mlua_event_set_nolock(st->stopped_event);
+    mlua_event_unlock(save);
 }
 
 static int mod_launch_core1(lua_State* ls) {
     uint const core = 1;
-    if (get_core_num() == core)
-        luaL_error(ls, "cannot launch core 1 from itself");
-    lua_State* ls1 = mlua_new_interpreter();
-    uint32_t save = spin_lock_blocking(mlua_lock);
-    bool busy = interpreters[core - 1] != NULL;
-    if (!busy) interpreters[core - 1] = ls1;
-    spin_unlock(mlua_lock, save);
-    if (busy) {
-        lua_close(ls1);
-        return luaL_error(ls, "core 1 is already running an interpreter");
+    if (get_core_num() == core) {
+        return luaL_error(ls, "cannot launch core %d from itself", core);
     }
     size_t len;
-    lua_pushlstring(ls1, luaL_checklstring(ls, 1, &len), len);
-    lua_pushstring(ls1, luaL_optstring(ls, 2, "main"));
-    multicore_launch_core1(&launch_core1);
+    char const* module = luaL_checklstring(ls, 1, &len);
+    char const* fn = luaL_optstring(ls, 2, "main");
+
+    // Set up the shutdown request event.
+    CoreState* st = &core_state[core - 1];
+    char const* err = mlua_event_claim_core(&st->shutdown_event, core);
+    if (err == mlua_event_err_already_claimed) {
+        return luaL_error(ls, "core %d is already running an interpreter",
+                          core);
+    } else if (err != NULL) {
+        return luaL_error(ls, "multicore: %s", err);
+    }
+
+    // Create a new interpreter.
+    st->ls = mlua_new_interpreter();
+    st->shutdown = false;
+
+    // Launch the interpreter in the other core.
+    lua_pushlstring(st->ls, module, len);
+    lua_pushstring(st->ls, fn);
+    multicore_launch_core1(&launch_core);
     return 0;
 }
 
-MLUA_FUNC_0_0(mod_, multicore_, reset_core1)
+static int try_stopped(lua_State* ls, bool timeout) {
+    CoreState* st = (CoreState*)lua_touserdata(ls, -1);
+    uint32_t save = mlua_event_lock();
+    bool stopped = st->shutdown_event == MLUA_EVENT_UNSET;
+    mlua_event_unlock(save);
+    if (!stopped) return -1;
+    mlua_event_unclaim(ls, &st->stopped_event);
+    multicore_reset_core1();
+    return 0;
+}
+
+static int mod_reset_core1(lua_State* ls) {
+    uint const core = 1;
+    if (get_core_num() == core) {
+        return luaL_error(ls, "cannot reset core %d from itself", core);
+    }
+    CoreState* st = &core_state[core - 1];
+
+    // Trigger the shutdown event if the other core is running an interpreter.
+    uint32_t save = mlua_event_lock();
+    bool running = st->shutdown_event != MLUA_EVENT_UNSET;
+    if (running) {
+        st->shutdown = true;
+        mlua_event_set_nolock(st->shutdown_event);
+    }
+    mlua_event_unlock(save);
+    if (!running) {
+        multicore_reset_core1();
+        return 0;
+    }
+
+    // Wait for the interpreter to terminate.
+    char const* err = mlua_event_claim(&st->stopped_event);
+    if (err != NULL) return luaL_error(ls, "multicore: %s", err);
+    lua_pushlightuserdata(ls, st);
+    return mlua_event_wait(ls, st->stopped_event, &try_stopped, 0);
+}
+
+static int try_shutdown(lua_State* ls, bool timeout) {
+    CoreState* st = &core_state[get_core_num() - 1];
+    uint32_t save = mlua_event_lock();
+    bool shutdown = st->shutdown;
+    mlua_event_unlock(save);
+    return shutdown ? 0 : -1;
+}
+
+static int mod_wait_shutdown(lua_State* ls) {
+    uint core = get_core_num();
+    if (core == 0) return luaL_error(ls, "cannot wait for shutdown on core 0");
+    return mlua_event_wait(ls, core_state[core - 1].shutdown_event,
+                           &try_shutdown, 0);
+}
+
+typedef struct FifoState {
+    MLuaEvent event;
+    uint8_t status;
+} FifoState;
+
+static FifoState fifo_state[NUM_CORES];
+
+static void __time_critical_func(handle_sio_irq)(void) {
+    uint core = get_core_num();
+    FifoState* st = &fifo_state[core];
+    uint32_t status = multicore_fifo_get_status();
+    multicore_fifo_clear_irq();
+    st->status |= status & (SIO_FIFO_ST_ROE_BITS | SIO_FIFO_ST_WOF_BITS);
+    if (status & SIO_FIFO_ST_VLD_BITS) {
+        irq_set_enabled(SIO_IRQ_PROC0 + core, false);
+        mlua_event_set(st->event);
+    }
+}
+
+static int mod_fifo_enable_irq(lua_State* ls) {
+    uint core = get_core_num();
+    char const* err = mlua_event_enable_irq(
+        ls, &fifo_state[core].event, SIO_IRQ_PROC0 + core,
+        &handle_sio_irq, 1, -1);
+    if (err != NULL) return luaL_error(ls, "SIO[%d]: %s", core, err);
+    return 0;
+}
+
+static int try_fifo_pop(lua_State* ls, bool timeout) {
+    if (timeout) return 0;
+    if (multicore_fifo_rvalid()) {
+        lua_pushinteger(ls, multicore_fifo_pop_blocking());
+        return 1;
+    }
+    irq_set_enabled(SIO_IRQ_PROC0 + get_core_num(), true);
+    return -1;
+}
+
+static int fifo_pop(lua_State* ls, int index) {
+    if (mlua_yield_enabled()) {
+        return mlua_event_wait(ls, fifo_state[get_core_num()].event,
+                               &try_fifo_pop, index);
+    }
+    lua_pushinteger(ls, multicore_fifo_pop_blocking());
+    return 1;
+}
+
+static int mod_fifo_pop_blocking(lua_State* ls) {
+    return fifo_pop(ls, 0);
+}
+
+static int mod_fifo_pop_timeout_us(lua_State* ls) {
+    mlua_push_int64(ls, to_us_since_boot(make_timeout_time_us(
+        mlua_check_int64(ls, 1))));
+    lua_replace(ls, 1);
+    return fifo_pop(ls, 1);
+}
+
+// TODO: Implement interruptible fifo_push_*() with polling
+// TODO: fifo_clear_irq should clear the saved status as well
+// TODO: fifo_get_status should return the combined status
+
+MLUA_FUNC_1_0(mod_, multicore_, fifo_rvalid, lua_pushboolean)
+MLUA_FUNC_1_0(mod_, multicore_, fifo_wready, lua_pushboolean)
+MLUA_FUNC_0_1(mod_, multicore_, fifo_push_blocking, luaL_checkinteger)
+MLUA_FUNC_1_2(mod_, multicore_, fifo_push_timeout_us, lua_pushboolean,
+              luaL_checkinteger, mlua_check_int64)
+MLUA_FUNC_0_0(mod_, multicore_, fifo_drain)
+MLUA_FUNC_0_0(mod_, multicore_, fifo_clear_irq)
+MLUA_FUNC_1_0(mod_, multicore_, fifo_get_status, lua_pushinteger)
 
 static MLuaSym const module_syms[] = {
     MLUA_SYM_F(reset_core1, mod_),
     MLUA_SYM_F(launch_core1, mod_),
+    MLUA_SYM_F(wait_shutdown, mod_),
+    MLUA_SYM_F(fifo_rvalid, mod_),
+    MLUA_SYM_F(fifo_wready, mod_),
+    MLUA_SYM_F(fifo_push_blocking, mod_),
+    MLUA_SYM_F(fifo_push_timeout_us, mod_),
+    MLUA_SYM_F(fifo_pop_blocking, mod_),
+    MLUA_SYM_F(fifo_pop_timeout_us, mod_),
+    MLUA_SYM_F(fifo_drain, mod_),
+    MLUA_SYM_F(fifo_clear_irq, mod_),
+    MLUA_SYM_F(fifo_get_status, mod_),
+    MLUA_SYM_F(fifo_enable_irq, mod_),
 };
 
+static __attribute__((constructor)) void init(void) {
+    for (uint core = 0; core < NUM_CORES; ++core) {
+        if (core != 0) {
+            core_state[core - 1].shutdown_event = MLUA_EVENT_UNSET;
+            core_state[core - 1].stopped_event = MLUA_EVENT_UNSET;
+        }
+        fifo_state[core].event = MLUA_EVENT_UNSET;
+    }
+}
+
 int luaopen_pico_multicore(lua_State* ls) {
+    mlua_event_require(ls);
+    mlua_require(ls, "mlua.int64", false);
+
     mlua_new_module(ls, 0, module_syms);
     return 1;
 }
--- lib/pico.multicore.test.lua
+++ lib/pico.multicore.test.lua
@@ -1,5 +1,49 @@
 _ENV = mlua.Module(...)
 
-function test_something(t)
-    -- TODO
+local debug = require 'debug'
+local io = require 'mlua.io'
+local thread = require 'mlua.thread'
+local multicore = require 'pico.multicore'
+local string = require 'string'
+
+local module_name = ...
+local core1 = {}
+
+local function watch_shutdown()
+    thread.start(function()
+        multicore.wait_shutdown()
+        thread.shutdown()
+    end)
+end
+
+function test_core1(t)
+    multicore.launch_core1(module_name, 'core1_wait_shutdown')
+    multicore.reset_core1()
+end
+
+function core1_wait_shutdown()
+    watch_shutdown()
+end
+
+function test_fifo(t)
+    -- TODO: Test variants with timeouts
+    -- TODO: Test other functions
+    multicore.launch_core1(module_name, 'core1_fifo_echo')
+    t:cleanup(multicore.reset_core1)
+
+    multicore.fifo_enable_irq()
+    t:cleanup(function() multicore.fifo_enable_irq(false) end)
+    for i = 1, 10 do
+        multicore.fifo_push_blocking(i)
+        t:expect(t:expr(multicore).fifo_pop_blocking()):eq(i)
+    end
+end
+
+function core1_fifo_echo()
+    watch_shutdown()
+    multicore.fifo_enable_irq()
+    local done<close> = function() multicore.fifo_enable_irq(false) end
+    while true do
+        multicore.fifo_push_blocking(multicore.fifo_pop_blocking())
+    end
 end
