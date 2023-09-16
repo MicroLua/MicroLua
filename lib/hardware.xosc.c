38bd5a324fd3532a5cc7f89997aa0d05889f3f9b
Remy Blank <remy@c-space.org>
2023-09-16 23:19:47 +0200

    hardware.xosc: Add module.

---
+++ lib/hardware.xosc.c
@@ -0,0 +1,20 @@
+#include "hardware/xosc.h"
+
+#include "lua.h"
+#include "lauxlib.h"
+#include "mlua/module.h"
+
+MLUA_FUNC_0_0(mod_, xosc_, init)
+MLUA_FUNC_0_0(mod_, xosc_, disable)
+MLUA_FUNC_0_0(mod_, xosc_, dormant)
+
+MLUA_SYMBOLS(module_syms) = {
+    MLUA_SYM_F(init, mod_),
+    MLUA_SYM_F(disable, mod_),
+    MLUA_SYM_F(dormant, mod_),
+};
+
+MLUA_OPEN_MODULE(hardware.xosc) {
+    mlua_new_module(ls, 0, module_syms);
+    return 1;
+}
---
+++ lib/hardware.xosc.test.lua
@@ -0,0 +1,11 @@
+_ENV = mlua.Module(...)
+
+local xosc = require 'hardware.xosc'
+
+function test_disable(t)
+    -- TODO
+end
+
+function test_dormant(t)
+    -- TODO
+end
