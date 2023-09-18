686e3dd04aef5ec28cd4b7a8cd9f9b74bbbb11b3
Remy Blank <remy@c-space.org>
2023-09-18 21:16:04 +0200

    hardware.watchdog: Add module.

---
+++ lib/hardware.watchdog.c
@@ -0,0 +1,28 @@
+#include "hardware/watchdog.h"
+
+#include "lua.h"
+#include "lauxlib.h"
+#include "mlua/module.h"
+#include "mlua/util.h"
+
+MLUA_FUNC_0_1(mod_, watchdog_, start_tick, luaL_checkinteger)
+MLUA_FUNC_0_0(mod_, watchdog_, update)
+MLUA_FUNC_0_2(mod_, watchdog_, enable, luaL_checkinteger, mlua_to_cbool)
+MLUA_FUNC_1_0(mod_, watchdog_, caused_reboot, lua_pushboolean)
+MLUA_FUNC_1_0(mod_, watchdog_, enable_caused_reboot, lua_pushboolean)
+MLUA_FUNC_1_0(mod_, watchdog_, get_count, lua_pushinteger)
+
+MLUA_SYMBOLS(module_syms) = {
+    // watchdog_reboot: Not useful in Lua
+    MLUA_SYM_F(start_tick, mod_),
+    MLUA_SYM_F(update, mod_),
+    MLUA_SYM_F(enable, mod_),
+    MLUA_SYM_F(caused_reboot, mod_),
+    MLUA_SYM_F(enable_caused_reboot, mod_),
+    MLUA_SYM_F(get_count, mod_),
+};
+
+MLUA_OPEN_MODULE(hardware.watchdog) {
+    mlua_new_module(ls, 0, module_syms);
+    return 1;
+}
---
+++ lib/hardware.watchdog.test.lua
@@ -0,0 +1,51 @@
+_ENV = mlua.Module(...)
+
+local base = require 'hardware.base'
+local addressmap = require 'hardware.regs.addressmap'
+local psm = require 'hardware.regs.psm'
+local regs = require 'hardware.regs.watchdog'
+local watchdog = require 'hardware.watchdog'
+local time = require 'pico.time'
+
+-- Return true iff the watchdog is enabled.
+local function is_enabled()
+    return (base.read32(addressmap.WATCHDOG_BASE + regs.CTRL_OFFSET)
+            & regs.CTRL_ENABLE_BITS) ~= 0
+end
+
+-- Return the value of the SCRATCH4 register.
+local function scratch4()
+    return base.read32(addressmap.WATCHDOG_BASE + regs.SCRATCH4_OFFSET)
+end
+
+-- Prevent the watchdog from resetting any peripherals.
+local function inhibit_reset()
+    base.hw_clear_bits(addressmap.PSM_BASE + psm.WDSEL_OFFSET,
+                       psm.WDSEL_BITS)
+end
+
+-- Disable the watchdog.
+local function disable()
+    base.hw_clear_bits(addressmap.WATCHDOG_BASE + regs.CTRL_OFFSET,
+                       regs.CTRL_ENABLE_BITS)
+    base.write32(addressmap.WATCHDOG_BASE + regs.SCRATCH4_OFFSET, 0)
+    inhibit_reset()
+end
+
+function test_watchdog(t)
+    t:cleanup(disable)
+
+    disable()
+    t:expect(is_enabled()):label("watchdog enabled"):eq(false)
+    t:expect(t:expr(watchdog).enable_caused_reboot()):eq(false)
+
+    watchdog.enable(1, true)
+    inhibit_reset()
+    t:expect(is_enabled()):label("watchdog enabled"):eq(true)
+    t:expect(t:expr(watchdog).get_count()):eq(1000)
+    t:expect(scratch4()):label("SCRATCH4"):eq(0x6ab73121)
+    watchdog.update()
+    time.sleep_ms(1)
+    t:expect(t:expr(watchdog).caused_reboot()):eq(true)
+    t:expect(t:expr(watchdog).enable_caused_reboot()):eq(true)
+end
