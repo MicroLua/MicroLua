f42d989279ba73b88f1841585c7a519bd6ac7889
Remy Blank <remy@c-space.org>
2023-09-21 16:00:24 +0200

    hardware.resets: Add module.

---
+++ lib/hardware.resets.c
@@ -0,0 +1,21 @@
+#include "hardware/resets.h"
+
+#include "lua.h"
+#include "lauxlib.h"
+#include "mlua/module.h"
+#include "mlua/util.h"
+
+MLUA_FUNC_0_1(mod_,, reset_block, luaL_checkinteger)
+MLUA_FUNC_0_1(mod_,, unreset_block, luaL_checkinteger)
+MLUA_FUNC_0_1(mod_,, unreset_block_wait, luaL_checkinteger)
+
+MLUA_SYMBOLS(module_syms) = {
+    MLUA_SYM_F(reset_block, mod_),
+    MLUA_SYM_F(unreset_block, mod_),
+    MLUA_SYM_F(unreset_block_wait, mod_),
+};
+
+MLUA_OPEN_MODULE(hardware.resets) {
+    mlua_new_module(ls, 0, module_syms);
+    return 1;
+}
