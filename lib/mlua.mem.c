b4596ec9e98a2686e6547313417ea9891ed1778d
Remy Blank <remy@c-space.org>
2023-09-21 12:34:42 +0200

    mlua.mem: Add module.

---
+++ lib/mlua.mem.c
@@ -0,0 +1,109 @@
+#include <string.h>
+
+#include "lua.h"
+#include "lauxlib.h"
+#include "mlua/module.h"
+
+static int read_mem(lua_State* ls, void const* src, size_t size) {
+    if (size <= 0) {
+        lua_pushliteral(ls, "");
+        return 1;
+    }
+    luaL_Buffer buf;
+    void* dest = (void*)luaL_buffinitsize(ls, &buf, size);
+    memcpy(dest, src, size);
+    luaL_pushresultsize(&buf, size);
+    return 1;
+}
+
+static char const Buffer_name[] = "mlua.mem.Buffer";
+
+static inline void* check_Buffer(lua_State* ls, int arg) {
+    return luaL_checkudata(ls, arg, Buffer_name);
+}
+
+static int Buffer_addr(lua_State* ls) {
+    lua_pushinteger(ls, (uintptr_t)check_Buffer(ls, 1));
+    return 1;
+}
+
+static int Buffer_clear(lua_State* ls) {
+    void* buf = check_Buffer(ls, 1);
+    lua_Unsigned size = lua_rawlen(ls, 1);
+    memset(buf, luaL_optinteger(ls, 2, 0), size);
+    return 0;
+}
+
+static int Buffer_read(lua_State* ls) {
+    void const* buf = check_Buffer(ls, 1);
+    lua_Unsigned size = lua_rawlen(ls, 1);
+    lua_Unsigned offset = luaL_optinteger(ls, 2, 0);
+    luaL_argcheck(ls, offset <= size, 2, "out of bounds");
+    lua_Unsigned len = luaL_optinteger(ls, 3, size - offset);
+    luaL_argcheck(ls, offset + len <= size, 3, "out of bounds");
+    return read_mem(ls, buf + offset, len);
+}
+
+static int Buffer_write(lua_State* ls) {
+    void* buf = check_Buffer(ls, 1);
+    lua_Unsigned size = lua_rawlen(ls, 1);
+    size_t len;
+    void const* src = (void const*)luaL_checklstring(ls, 2, &len);
+    lua_Unsigned offset = luaL_optinteger(ls, 3, 0);
+    luaL_argcheck(ls, offset + len <= size, 3, "out of bounds");
+    memcpy(buf + offset, src, len);
+    return 0;
+}
+
+static int Buffer___len(lua_State* ls) {
+    lua_pushinteger(ls, lua_rawlen(ls, 1));
+    return 1;
+}
+
+MLUA_SYMBOLS(Buffer_syms) = {
+    MLUA_SYM_F(addr, Buffer_),
+    MLUA_SYM_F(clear, Buffer_),
+    MLUA_SYM_F(read, Buffer_),
+    MLUA_SYM_F(write, Buffer_),
+};
+
+MLUA_SYMBOLS_NOHASH(Buffer_syms_nh) = {
+    MLUA_SYM_F_NH(__len, Buffer_),
+};
+
+static int mod_read(lua_State* ls) {
+    return read_mem(ls, (void const*)luaL_checkinteger(ls, 1),
+                    luaL_optinteger(ls, 2, 1));
+}
+
+static int mod_write(lua_State* ls) {
+    void* ptr = (void*)luaL_checkinteger(ls, 1);
+    size_t len;
+    void const* src = (void const*)luaL_checklstring(ls, 2, &len);
+    memcpy(ptr, src, len);
+    return 0;
+}
+
+static int mod_alloc(lua_State* ls) {
+    lua_newuserdatauv(ls, luaL_checkinteger(ls, 1), 0);
+    luaL_getmetatable(ls, Buffer_name);
+    lua_setmetatable(ls, -2);
+    return 1;
+}
+
+MLUA_SYMBOLS(module_syms) = {
+    MLUA_SYM_F(read, mod_),
+    MLUA_SYM_F(write, mod_),
+    MLUA_SYM_F(alloc, mod_),
+};
+
+MLUA_OPEN_MODULE(mlua.mem) {
+    // Create the module.
+    mlua_new_module(ls, 0, module_syms);
+
+    // Create the Buffer class.
+    mlua_new_class(ls, Buffer_name, Buffer_syms, true);
+    mlua_set_fields(ls, Buffer_syms_nh);
+    lua_pop(ls, 1);
+    return 1;
+}
---
+++ lib/mlua.mem.test.lua
@@ -0,0 +1,39 @@
+_ENV = mlua.Module(...)
+
+local mem = require 'mlua.mem'
+local string = require 'string'
+
+function test_Buffer(t)
+    local buf = mem.alloc(10)
+    t:expect(buf:addr() ~= 0, "buffer address is 0")
+    t:expect(#buf):label("#buf"):eq(10)
+    t:expect(tostring(buf)):label("tostring(buf)")
+        :eq(('mlua.mem.Buffer: %08x'):format(buf:addr()))
+
+    buf:clear()
+    t:expect(t:expr(buf):read()):eq('\0\0\0\0\0\0\0\0\0\0')
+    buf:clear(('_'):byte())
+    t:expect(t:expr(buf):read()):eq('__________')
+
+    buf:write('abc')
+    t:expect(t:expr(buf):read()):eq('abc_______')
+    buf:write('def', 3)
+    t:expect(t:expr(buf):read()):eq('abcdef____')
+    t:expect(t:expr(buf):write('x', 10)):raises("out of bounds")
+
+    t:expect(t:expr(buf):read(3)):eq('def____')
+    t:expect(t:expr(buf):read(3, 4)):eq('def_')
+    t:expect(t:expr(buf):read(11)):raises("out of bounds")
+    t:expect(t:expr(buf):read(10, 1)):raises("out of bounds")
+end
+
+function test_read_write(t)
+    local buf = mem.alloc(26)
+    for _, test in ipairs{
+        'abcdefghijklmnopqrstuvwxyz',
+        '1234567890',
+    } do
+        mem.write(buf:addr(), test)
+        t:expect(t:expr(mem).read(buf:addr(), #test)):eq(test)
+    end
+end
