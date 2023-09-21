5b2290004413dd27afd45c4c04cde01945fda02b
Remy Blank <remy@c-space.org>
2023-09-21 12:36:19 +0200

    hardware.flash: Add module.

---
+++ lib/hardware.flash.c
@@ -0,0 +1,50 @@
+#include "hardware/flash.h"
+
+#include "lua.h"
+#include "lauxlib.h"
+#include "mlua/module.h"
+
+int mod_range_program(lua_State* ls) {
+    uint32_t offs = luaL_checkinteger(ls, 1);
+    size_t len;
+    uint8_t const* data = (uint8_t const*)luaL_checklstring(ls, 2, &len);
+    flash_range_program(offs, data, len);
+    return 0;
+}
+
+int mod_get_unique_id(lua_State* ls) {
+    luaL_Buffer buf;
+    uint8_t* dest = (uint8_t*)luaL_buffinitsize(ls, &buf, 8);
+    flash_get_unique_id(dest);
+    luaL_pushresultsize(&buf, 8);
+    return 1;
+}
+
+int mod_do_cmd(lua_State* ls) {
+    size_t len;
+    uint8_t const* txbuf = (uint8_t const*)luaL_checklstring(ls, 1, &len);
+    luaL_Buffer buf;
+    uint8_t* dest = (uint8_t*)luaL_buffinitsize(ls, &buf, len);
+    flash_do_cmd(txbuf, dest, len);
+    luaL_pushresultsize(&buf, len);
+    return 1;
+}
+
+MLUA_FUNC_0_2(mod_, flash_, range_erase, luaL_checkinteger, luaL_checkinteger)
+
+MLUA_SYMBOLS(module_syms) = {
+    MLUA_SYM_V(PAGE_SIZE, integer, FLASH_PAGE_SIZE),
+    MLUA_SYM_V(SECTOR_SIZE, integer, FLASH_SECTOR_SIZE),
+    MLUA_SYM_V(BLOCK_SIZE, integer, FLASH_BLOCK_SIZE),
+    MLUA_SYM_V(UNIQUE_ID_SIZE_BYTES, integer, FLASH_UNIQUE_ID_SIZE_BYTES),
+
+    MLUA_SYM_F(range_erase, mod_),
+    MLUA_SYM_F(range_program, mod_),
+    MLUA_SYM_F(get_unique_id, mod_),
+    MLUA_SYM_F(do_cmd, mod_),
+};
+
+MLUA_OPEN_MODULE(hardware.flash) {
+    mlua_new_module(ls, 0, module_syms);
+    return 1;
+}
---
+++ lib/hardware.flash.test.lua
@@ -0,0 +1,46 @@
+_ENV = mlua.Module(...)
+
+local flash = require 'hardware.flash'
+local addressmap = require 'hardware.regs.addressmap'
+local sync = require 'hardware.sync'
+local mem = require 'mlua.mem'
+local pico = require 'pico'
+local unique_id = require 'pico.unique_id'
+local string = require 'string'
+
+function test_erase_program(t)
+    local offs = (pico.FLASH_SIZE_BYTES - flash.SECTOR_SIZE)
+                 & ~(flash.SECTOR_SIZE - 1)
+    if offs < pico.flash_binary_end - pico.flash_binary_start then
+        t:skip("no free sector in flash memory")
+    end
+
+    -- Erase the last sector in flash.
+    local save = sync.save_and_disable_interrupts()
+    flash.range_erase(offs, flash.SECTOR_SIZE)
+    sync.restore_interrupts(save)
+    t:expect(t:expr(mem).read(addressmap.XIP_BASE + offs, flash.SECTOR_SIZE))
+        :eq(('\xff'):rep(flash.SECTOR_SIZE))
+
+    -- Program and verify the first page of the last sector.
+    local data = ('abcdefghijklmnopqrstuvwxyz012345'):rep(flash.PAGE_SIZE // 32)
+    local save = sync.save_and_disable_interrupts()
+    flash.range_program(offs, data)
+    sync.restore_interrupts(save)
+    t:expect(t:expr(mem).read(addressmap.XIP_BASE + offs, #data)):eq(data)
+end
+
+function test_get_unique_id(t)
+    local save = sync.save_and_disable_interrupts()
+    local id = flash.get_unique_id()
+    sync.restore_interrupts(save)
+    t:expect(id):label("get_unique_id()"):eq(unique_id.get_unique_board_id())
+end
+
+function test_do_cmd(t)
+    -- Issue a RUID command.
+    local save = sync.save_and_disable_interrupts()
+    local rx = flash.do_cmd('\x4b123412345678')
+    sync.restore_interrupts(save)
+    t:expect(rx:sub(6)):label("do_cmd()"):eq(unique_id.get_unique_board_id())
+end
