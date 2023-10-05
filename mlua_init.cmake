7f29033eee0c5e2ea98167a183b2bb0ccd97d423
Remy Blank <remy@c-space.org>
2023-10-05 11:35:20 +0200

    Remove unnecessary comments.

--- mlua_init.cmake
+++ mlua_init.cmake
@@ -1,14 +1,10 @@
-# if(NOT TARGET _mlua_init_included)
-    add_library(_mlua_init_included INTERFACE)
+include("${MLUA_PATH}/pico_sdk_import.cmake")
 
-    include("${MLUA_PATH}/pico_sdk_import.cmake")
-
-    macro(mlua_init)
-        if(NOT CMAKE_PROJECT_NAME)
-            message(WARNING
-                "mlua_init() should be called after the project is created")
-        endif()
-        pico_sdk_init()
-        add_subdirectory("${MLUA_PATH}" MicroLua)
-    endmacro()
-# endif()
+macro(mlua_init)
+    if(NOT CMAKE_PROJECT_NAME)
+        message(WARNING
+            "mlua_init() should be called after the project is created")
+    endif()
+    pico_sdk_init()
+    add_subdirectory("${MLUA_PATH}" MicroLua)
+endmacro()
