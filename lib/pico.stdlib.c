683e8cfa00c96f7d42fb8b6a39054f7e7f8a9d81
Remy Blank <remy@c-space.org>
2023-10-07 10:29:02 +0200

    pico.stdlib: Add a few missing symbols.

--- lib/pico.stdlib.c
+++ lib/pico.stdlib.c
@@ -26,6 +26,18 @@ MLUA_FUNC_1_2(mod_,, set_sys_clock_khz, lua_pushboolean, luaL_checkinteger,
               mlua_to_cbool)
 
 MLUA_SYMBOLS(module_syms) = {
+    MLUA_SYM_V(DEFAULT_LED_PIN_INVERTED, boolean, PICO_DEFAULT_LED_PIN_INVERTED),
+#ifdef PICO_DEFAULT_WS2812_PIN
+    MLUA_SYM_V(DEFAULT_WS2812_PIN, integer, PICO_DEFAULT_WS2812_PIN),
+#else
+    MLUA_SYM_V(DEFAULT_WS2812_PIN, boolean, false),
+#endif
+#ifdef PICO_DEFAULT_WS2812_POWER_PIN
+    MLUA_SYM_V(DEFAULT_WS2812_POWER_PIN, integer, PICO_DEFAULT_WS2812_POWER_PIN),
+#else
+    MLUA_SYM_V(DEFAULT_WS2812_POWER_PIN, boolean, false),
+#endif
+
     MLUA_SYM_F(setup_default_uart, mod_),
     MLUA_SYM_F(set_sys_clock_48mhz, mod_),
     MLUA_SYM_F(set_sys_clock_pll, mod_),
