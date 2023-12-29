0cae6f9ea7f0bb0865cc1e36cfebc51abc7d003d
Remy Blank <remy@c-space.org>
2023-12-29 19:24:18 +0100

    mlua.shell: Add the underscore as a safe character.

--- lib/common/mlua.shell.lua
+++ lib/common/mlua.shell.lua
@@ -10,6 +10,6 @@ local string = require 'string'
 -- Return a shell-escaped version of the argument.
 function quote(s)
     if s == '' then return "''" end
-    if not s:find('[^%w@%%+=:,./-]') then return s end
+    if not s:find('[^%w_@%%+=:,./-]') then return s end
     return ("'%s'"):format(s:gsub("'", "'\"'\"'"))
 end
