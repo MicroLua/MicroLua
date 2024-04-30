41893408c2b764071893c41a7e563f12f939a243
Remy Blank <remy@c-space.org>
2024-04-30 15:18:09 +0200

    Pull up the default UART pins in tests.
     - This avoids spurious input when the pins aren't connected.

--- lib/pico/00_setup.test.lua
+++ lib/pico/00_setup.test.lua
@@ -3,12 +3,27 @@
 
 _ENV = module(...)
 
+local gpio = require 'hardware.gpio'
 local uart = require 'hardware.uart'
+local testing_uart = require 'mlua.testing.uart'
+local stdio = require 'pico.stdio'
 local stdlib = require 'pico.stdlib'
 
 function set_up(t)
+    -- Pull up the default UART Tx & Rx pins, to avoid spurious input if they
+    -- aren't connected.
+    local u = uart.default
+    if u then
+        local tx, rx = testing_uart.find_pins(u)
+        if rx then
+            gpio.pull_up(tx)
+            gpio.pull_up(rx)
+            while stdio.getchar_timeout_us(1000) >= 0 do end
+        end
+        u:tx_wait_blocking()
+    end
+
     -- Set a faster clock speed.
-    if uart.default then uart.default:tx_wait_blocking() end
     stdlib.set_sys_clock_khz(250000, true)
-    stdlib.setup_default_uart()
+    if u then u:set_baudrate(uart.DEFAULT_BAUD_RATE) end
 end
