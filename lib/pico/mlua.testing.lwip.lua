58d755de9cbf3824ed95bc16a628a15ddb08144d
Remy Blank <remy@c-space.org>
2024-06-28 17:12:40 +0200

    pico.lwip.udp: Add a send test.

--- lib/pico/mlua.testing.lwip.lua
+++ lib/pico/mlua.testing.lwip.lua
@@ -7,10 +7,11 @@ local config = require 'mlua.config'
 local io = require 'mlua.io'
 local mem = require 'mlua.mem'
 local oo = require 'mlua.oo'
+local testing = require 'mlua.testing'
 local time = require 'mlua.time'
 local lwip = require 'pico.lwip'
 local dns = require 'pico.lwip.dns'
-local stats = try(require, 'pico.lwip.stats')
+local stats = require 'pico.lwip.stats'
 local tcp = require 'pico.lwip.tcp'
 local string = require 'string'
 local table = require 'table'
@@ -87,8 +88,7 @@ function Control:wait_close(t)
     t:expect(resp):label("done"):eq('DONE\n')
 end
 
-local testing = require 'mlua.testing'
-if stats and not testing.overrides[...] then
+if not testing.overrides[...] then
     testing.overrides[...] = true
     local Test = testing.Test
 
@@ -105,18 +105,17 @@ if stats and not testing.overrides[...] then
     local print_stats = Test._print_stats
     function Test:_print_stats(out)
         print_stats(self, out)
-        if not self._parent then return end
         if stats.mem then
             local st = stats.mem()
             io.fprintf(
-                out, "lwIP.mem: avail: %s B, used: %s B, max: %s B\n",
+                out, "lwIP mem: avail: %s B, used: %s B, max: %s B\n",
                 st.avail, st.used, st.max)
         end
         if stats.memp then
             for i = 0, stats.MEMP_COUNT - 1 do
                 local st, name = stats.memp(i)
                 io.fprintf(
-                    out, "lwIP.memp[%s]: avail: %s, used: %s, max: %s\n",
+                    out, "lwIP memp[%s]: avail: %s, used: %s, max: %s\n",
                     name or i, st.avail, st.used, st.max)
             end
         end
--- lib/pico/pico.lwip.udp.test.lua
+++ lib/pico/pico.lwip.udp.test.lua
@@ -9,14 +9,78 @@ local thread = require 'mlua.thread'
 local group = require 'mlua.thread.group'
 local time = require 'mlua.time'
 local lwip = require 'pico.lwip'
+local pbuf = require 'pico.lwip.pbuf'
 local udp = require 'pico.lwip.udp'
 local table = require 'table'
+
+function test_send(t)
+    for _, test in ipairs{
+        {"send", 'send', nil, {1234}, 64, 10, 10 * time.msec},
+        {"small", 'sendto', nil, {1234}, 64, 10, 10 * time.msec},
+        {"large", 'sendto', nil, {1234}, 1200, 10, 50 * time.msec},
+        {"many", 'sendto', nil, {1234}, 64, 100, 10 * time.msec},
+        {"fast", 'sendto', nil, {1234}, 8, 10, 1 * time.usec},
+        {"multi", 'sendto', nil, {1234, 5678, 910}, 64, 10, 10 * time.msec},
+        {"IPv4", 'sendto', testing_lwip.IPV4, {1234}, 64, 10, 10 * time.msec},
+        {"IPv6", 'sendto', testing_lwip.IPV6, {1234}, 64, 10, 10 * time.msec},
+    } do
+        local desc, fname, atype, ports = table.unpack(test, 1, 4)
+        t:run(desc, function(t)
+            local tg<close> = thread.Group()
+            for _, lport in ipairs(ports) do
+                tg:start(with_traceback(function()
+                    return run_send_test(t, fname, atype, lport,
+                                         table.unpack(test, 5))
+                end))
+            end
+        end)
+    end
+end
+
+function run_send_test(t, fname, atype, lport, size, count, interval)
+    local ctrl = testing_lwip.Control(t, atype)
+    local sock<close> = udp.new(nil, count)
+    sock:bind(nil, lport)
+    sock:connect(ctrl.addr, ctrl.port)
+
+    local received = testing_lwip.Set()
+    local recveiver<close> = thread.start(with_traceback(function()
+        local dl = time.deadline(count * interval + 1000 * time.msec)
+        while true do
+            local line = ctrl:recv(dl)
+            if line == '' then break end
+            local rsize, pid, ok = line:match('^RECV (%d+) (%d+) (.+)\n$')
+            t:expect(tonumber(rsize)):label("rsize"):eq(size)
+            t:expect(ok):label("data ok"):eq('OK')
+            if received:add(pid) == count then break end
+        end
+    end))
+    ctrl:send('udp_recv %s\n', lport)
+
+    local send, to = sock[fname], fname == 'sendto'
+    local sender<close> = thread.start(with_traceback(function()
+        for pid = 0, count - 1 do
+            local p<close> = pbuf.alloc(pbuf.TRANSPORT, size)
+            testing_lwip.generate_data(pid, size, p, 0)
+            local addr = to and ctrl.addr or nil
+            local port = to and ctrl.port or nil
+            local ok, err = send(sock, p, addr, port)
+            t:expect(ok):label("send() ok"):eq(true)
+            t:expect(err):label("send() err"):eq(nil)
+            time.sleep_for(interval)
+        end
+    end))
+
+    sender:join()
+    recveiver:join()
+    t:expect(#received):label("received"):eq(count)
+end
 
 function test_recv(t)
     for _, test in ipairs{
         {"recv", 'recv', nil, {1234}, 64, 10, 10 * time.msec},
         {"small", 'recvfrom', nil, {1234}, 64, 10, 10 * time.msec},
-        {"large", 'recvfrom', nil, {1234}, 2000, 10, 10 * time.msec},
+        {"large", 'recvfrom', nil, {1234}, 1200, 10, 10 * time.msec},
         {"many", 'recvfrom', nil, {1234}, 64, 100, 10 * time.msec},
         {"fast", 'recvfrom', nil, {1234}, 64, 10, 1 * time.usec},
         {"multi", 'recvfrom', nil, {1234, 5678, 910}, 64, 10, 10 * time.msec},
