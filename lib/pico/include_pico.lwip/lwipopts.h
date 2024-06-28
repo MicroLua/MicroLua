6fb13756316b4547fa2d709f67ba9bb136bb9b06
Remy Blank <remy@c-space.org>
2024-06-28 17:11:36 +0200

    lwip: Make memory pool sizes configurable.

--- lib/pico/include_pico.lwip/lwipopts.h
+++ lib/pico/include_pico.lwip/lwipopts.h
@@ -9,14 +9,6 @@
 #define LWIP_SOCKET                 0
 #define MEM_LIBC_MALLOC             0
 #define MEM_ALIGNMENT               4
-// TODO: Make some of the options below configurable
-#ifndef LWIP_MEM_SIZE
-#define LWIP_MEM_SIZE               4000
-#endif
-#define MEM_SIZE                    LWIP_MEM_SIZE
-#define MEMP_NUM_ARP_QUEUE          10
-#define MEMP_NUM_TCP_SEG            32
-#define PBUF_POOL_SIZE              24
 // #define ETH_PAD_SIZE                2
 #define TCP_MSS                     1460
 #define TCP_WND                     (8 * TCP_MSS)
@@ -36,17 +28,95 @@
 #define LWIP_DEBUG                  1
 #endif
 
-#ifndef LWIP_TCP
-#define LWIP_TCP                    0
-#endif
+// Subsystems.
 #ifndef LWIP_UDP
 #define LWIP_UDP                    0
 #endif
+#ifndef LWIP_TCP
+#define LWIP_TCP                    0
+#endif
 
+// Heap memory.
+#ifndef LWIP_MEM_SIZE
+#define LWIP_MEM_SIZE               4000
+#endif
+#define MEM_SIZE                    LWIP_MEM_SIZE
+
+// Memory pools.
+#ifndef LWIP_MEMP_NUM_PBUF
+#define LWIP_MEMP_NUM_PBUF              0
+#endif
+#define MEMP_NUM_PBUF                   LWIP_MEMP_NUM_PBUF
+
+#ifndef LWIP_MEMP_NUM_RAW_PCB
+#define LWIP_MEMP_NUM_RAW_PCB           4
+#endif
+#define MEMP_NUM_RAW_PCB                LWIP_MEMP_NUM_RAW_PCB
+
+#ifndef LWIP_MEMP_NUM_UDP_PCB
+#define LWIP_MEMP_NUM_UDP_PCB           4
+#endif
+#define MEMP_NUM_UDP_PCB                LWIP_MEMP_NUM_UDP_PCB
+
+#ifndef LWIP_MEMP_NUM_TCP_PCB
+#define LWIP_MEMP_NUM_TCP_PCB           5
+#endif
+#define MEMP_NUM_TCP_PCB                LWIP_MEMP_NUM_TCP_PCB
+
+#ifndef LWIP_MEMP_NUM_TCP_PCB_LISTEN
+#define LWIP_MEMP_NUM_TCP_PCB_LISTEN    5
+#endif
+#define MEMP_NUM_TCP_PCB_LISTEN         LWIP_MEMP_NUM_TCP_PCB_LISTEN
+
+#ifndef LWIP_MEMP_NUM_TCP_SEG
+#define LWIP_MEMP_NUM_TCP_SEG           32
+#endif
+#define MEMP_NUM_TCP_SEG                LWIP_MEMP_NUM_TCP_SEG
+
+#ifndef LWIP_MEMP_NUM_ALTCP_PCB
+#define LWIP_MEMP_NUM_ALTCP_PCB         LWIP_MEMP_NUM_TCP_PCB
+#endif
+#define MEMP_NUM_ALTCP_PCB              LWIP_MEMP_NUM_ALTCP_PCB
+
+#ifndef LWIP_MEMP_NUM_REASSDATA
+#define LWIP_MEMP_NUM_REASSDATA         5
+#endif
+#define MEMP_NUM_REASSDATA              LWIP_MEMP_NUM_REASSDATA
+
+#ifndef LWIP_MEMP_NUM_FRAG_PBUF
+#define LWIP_MEMP_NUM_FRAG_PBUF         15
+#endif
+#define MEMP_NUM_FRAG_PBUF              LWIP_MEMP_NUM_FRAG_PBUF
+
+#ifndef LWIP_MEMP_NUM_ARP_QUEUE
+#define LWIP_MEMP_NUM_ARP_QUEUE         10
+#endif
+#define MEMP_NUM_ARP_QUEUE              LWIP_MEMP_NUM_ARP_QUEUE
+
+#ifndef LWIP_MEMP_NUM_IGMP_GROUP
+#define LWIP_MEMP_NUM_IGMP_GROUP        10
+#endif
+#define MEMP_NUM_IGMP_GROUP             LWIP_MEMP_NUM_IGMP_GROUP
+
+#ifndef LWIP_MEMP_NUM_ND6_QUEUE
+#define LWIP_MEMP_NUM_ND6_QUEUE         20
+#endif
+#define MEMP_NUM_ND6_QUEUE              LWIP_MEMP_NUM_ND6_QUEUE
+
+#ifndef LWIP_MEMP_NUM_MLD6_GROUP
+#define LWIP_MEMP_NUM_MLD6_GROUP        4
+#endif
+#define MEMP_NUM_MLD6_GROUP             LWIP_MEMP_NUM_MLD6_GROUP
+
+#ifndef LWIP_PBUF_POOL_SIZE
+#define LWIP_PBUF_POOL_SIZE             16
+#endif
+#define PBUF_POOL_SIZE                  LWIP_PBUF_POOL_SIZE
+
+// Statistics.
 #ifndef LWIP_STATS
 #define LWIP_STATS                  0
 #endif
-
 #define LINK_STATS                  LWIP_LINK_STATS
 #define ETHARP_STATS                LWIP_ETHARP_STATS
 #define IPFRAG_STATS                LWIP_IPFRAG_STATS
